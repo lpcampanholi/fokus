3d3a81f1388cb5ee7ec4f7d9b8dc56d8334b0771
Luís Campanholi <lpcampanholi@gmail.com>
2024-07-19 16:32:30 -0300

    adicionando funcionalidade

--- exercicios/atividade lista de tarefas/script.js
+++ exercicios/atividade lista de tarefas/script.js
@@ -6,34 +6,42 @@ const listaDeTarefas = document.querySelector(".listaDeTarefas");
 
 const tarefas = [];
 
+function criarElementoTarefa(tarefa) {
+  const li = document.createElement("li");
+  li.classList.add("tarefa");
+  li.innerHTML = `
+    <p>${tarefa.descricao}</p>
+    <button>Excluir</button>
+  `;
+
+  listaDeTarefas.append(li);
+
+  console.log("criarElementoTarefa");
+};
+
 btn.addEventListener("click", () => {
   textArea.classList.toggle("hidden");
   btnSubmit.classList.toggle("hidden");
 });
 
+
+
 btnSubmit.addEventListener("click", (e) => {
   e.preventDefault();
   const tarefa = {
-  texto: textArea.value
+  descricao: textArea.value
   };
 
   tarefas.push(tarefa);
   localStorage.setItem("Tarefas", JSON.stringify(tarefas));
 
-  const tarefasSalvas = localStorage.getItem("Tarefas");
-
-  listaDeTarefas.innerHTML = `
-    <li class="tarefa">
-      <p>${textArea.value}</p>
-      <button>Excluir</button>
-    </li>
-  `
-
+  
+  const tarefasSalvas = JSON.parse(localStorage.getItem("Tarefas"));
+  
+  tarefasSalvas.forEach((tarefa) => {
+    criarElementoTarefa(tarefa);
+  });
   console.log(tarefasSalvas);
 
 });
-
-
-
-
 
--- script-crud.js
+++ script-crud.js
@@ -4,6 +4,28 @@ const textArea = document.querySelector(".app__form-textarea");
 
 const tarefas = [];
 
+function criarElementoTarefa(tarefa) {
+  const li = document.createElement("li");
+  li.classList.add("app__section-task-list-item");
+
+  const svg = document.createElement("svg");
+  svg.innerHTML = `
+    <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+      <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+    </svg>  
+  `;
+
+  const p = document.createElement("p");
+  p.classList.add("app__section-task-list-item-description");
+  p.textContent = "Estudando localStorage";
+
+  const button = document.createElement("button");
+  button.innerHTML = `
+    <img src="/imagens/edit.png">
+  `
+};
+
 botaoAdicionar.addEventListener("click", () => {
   formAdicionarTarefa.classList.toggle("hidden");
 });
